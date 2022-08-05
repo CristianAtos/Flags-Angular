a261b335ff975baf221f9a23d38938a9e239503d
Cristian Hernandez <cristian-alejandro.hernandez@atos.net>
2022-08-04 21:57:26 -0500

    Change scss

--- src/app/countries/countries.component.ts
+++ src/app/countries/countries.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpService } from '../http.service';
 import { Country } from './Country';
